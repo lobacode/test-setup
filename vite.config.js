b83a3368eb392a63e4aeea2b700ceebc9ac592f3
Lobacode <lobacode@gmail.com>
2024-10-15 15:17:56 +0700

    Splitting output

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
 import vituum from 'vituum';
 import liquidjs from '@vituum/vite-plugin-liquid';
-import { glob } from 'glob';
-
-const entries = glob.sync('./src/**/*.liquid').reduce((acc, path) => {
-  const name = path.split('/').pop().split('.').shift();
-  acc[name] = path;
-  return acc;
-}, {});
-
 
 export default defineConfig({
   css: {
@@ -24,4 +16,52 @@ export default defineConfig({
       root: 'src'
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (chunkInfo) => {
+          let outDir = '';
+
+          // Fonts
+          if (/(ttf|woff|woff2|eot)$/.test(chunkInfo.name)) {
+            outDir = 'fonts';
+          }
+
+          // SVG
+          if (/svg$/.test(chunkInfo.name)) {
+            outDir = 'svg';
+          }
+
+          // Images
+          if (/(png|jpg|jpeg|gif|webp)$/.test(chunkInfo.name)) {
+            outDir = 'images';
+          }
+
+          // Media
+          if (/(mp3|mp4|webm|ogg|wav|flac|aac)$/.test(chunkInfo.name)) {
+            outDir += 'media';
+          }
+
+          // JSON
+          if (/json$/.test(chunkInfo.name)) {
+            outDir = 'json';
+          }
+
+          // JS
+          if (/js$/.test(chunkInfo.name)) {
+            outDir = 'js';
+          }
+
+          // CSS
+          if (/css$/.test(chunkInfo.name)) {
+            outDir = 'css';
+          }
+
+          return `${outDir}/[name][extname]`;
+        },
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+      },
+    },
+  },
 });
